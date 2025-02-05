129a5916cf6c36c16525e6e22d7d500ee2cdd8a6
gghinzu <goksuzun@gmail.com>
2025-02-05 17:15:44 +0300

    docker is installed and postgresql is integrated into the project all the files containing local users array is updated with database structure all the errors are tried to be handled gorm is used and codes are revised according to it new dto's are added for creation and update

--- internal/domain/service/user_getall_service.go
+++ internal/domain/service/user_getall_service.go
@@ -2,10 +2,7 @@ package service
 
 import (
 	"firstGoProject/internal/domain/entity"
-	"gorm.io/gorm"
 )
-
-var db *gorm.DB
 
 // GetAllUsers gets all users using an instance of UserService
 // (implementation of the interface UserServicePort)
